5944f75b4cbacc97854bba79efae41ee3f86ce58
zstayman <z.stayman@gmail.com>
2014-03-20 11:49:27 -0400

    passes all tests

--- app/models/party.rb
+++ app/models/party.rb
@@ -3,4 +3,5 @@ class Party < ActiveRecord::Base
   has_many :past_songs, through: :played_songs, source: :songs
   has_many :queued_songs
   has_many :played_songs
+  validates :party_key, presence: true
 end
