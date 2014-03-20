333990deaa9342564ad98defdf1524ec190a69d0
Michael K. Wagner <michael.k.wagner@gmail.com>
2014-03-20 14:14:26 -0400

    Create initial Grooveshark Method

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,23 @@
 class Song < ActiveRecord::Base
+
+
+  def getGrooveshark(songstring)
+    client = Grooveshark::Client.new
+    session = client.session
+    song = client.search_songs(songstring).first
+    song_title = song.data["song_name"]
+
+  end   
+    
+  def addSong(songstring)
+
+    if Song.where(title: song.title) = []
+
+    end  
+
+    ##Step 1:  send value to Grooveshark
+    ##Step 2A:  IF value not in SONG DB Add. 
+    ##Step 2B:  Add Song to the Song Queue
+  end 
+
 end
