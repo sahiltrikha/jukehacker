a3f3470a33d2450c9813dfbbb30eec9bf0cd6f0c
renagadex2 <etan.berkowitz@gmail.com>
2014-03-20 18:29:45 -0400

    user can add a song via the internet to our db

--- app/assets/javascripts/songsearch.js
+++ app/assets/javascripts/songsearch.js
@@ -1,5 +1,11 @@
+var songFieldInput = $("input")
 $("form").submit(function(e) {
   e.preventDefault();
 
-  $.post("/queued_songs", {  })
+  $.post("/queued_songs", { songstring: songFieldInput.val(),  party_id: window.location.href.split("/")[4] 
+  
+  });
+
+  this.reset();
+
 } )
--- app/controllers/queued_songs_controller.rb
+++ app/controllers/queued_songs_controller.rb
@@ -1,8 +1,15 @@
 class QueuedSongsController < ApplicationController
-  include GroovesharkSearch
+  include GroovesharkSearchHelper
+
+  # before_action :load_user
   
   def create
-    
+    @user_party = Party.find_by(id: params[:party_id])
+    @songstring = params[:songstring]
+    @party_id = params[:party_id]
+    binding.pry
+    getGrooveshark("#{@songstring}", "#{@party_id}", "#{@user_party.id}")
+
   end 
 
   def get_queue
@@ -10,4 +17,11 @@ class QueuedSongsController < ApplicationController
     render json: @party.upcoming_songs
   end
 
+
+
+  private
+
+  # def load_user
+  #   return @user = User.find(params[:user_id])
+  # end
 end 
