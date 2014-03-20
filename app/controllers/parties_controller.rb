1ca341348adc7e953149961862b334c0acc915b6
zstayman <z.stayman@gmail.com>
2014-03-20 11:01:21 -0400

    create action works

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -1,4 +1,18 @@
 class PartiesController < ApplicationController
   def new
+    @party = Party.new
+  end
+
+  def create
+    @party = Party.create(party_params)
+    @party.party_expiry = @party.created_at + 6.hours
+    @party.save
+    redirect_to "/party-dashboard/#{@party.id}"
+  end
+
+  private
+
+  def party_params
+    params.require(:party).permit(:party_key)
   end
 end
--- spec/features/host_can_create_a_party.rb
+++ spec/features/host_can_create_a_party.rb
@@ -4,7 +4,7 @@ describe "host can create a party" do
 
 	it "creates a party" do
 		visit('/partycreate')
-		fill_in 'party_key', with: "Test Party"
+		fill_in 'party_party_key', with: "Test Party"
     click_button 'Rock On'
     expect(page).to have_content("Test Party")
 	end
