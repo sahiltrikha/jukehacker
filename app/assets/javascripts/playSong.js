1be0edca9ee4ff0ec79c5566893c09a95cd81b8f
zstayman <z.stayman@gmail.com>
2014-03-22 14:41:56 -0400

    player works

--- app/assets/javascripts/playSong.js
+++ app/assets/javascripts/playSong.js
@@ -1,9 +1,12 @@
 var playSong = function(){
-  $("button").disabled = true;
+  console.log("DISABLED");
   $("#player").children().remove()
   $.getJSON('/getnextsong', function(song){
+    $("#start-the-party").attr("disabled", "disabled");
     if(song == "DONE"){
-      return false;
+      console.log("ENABLED");
+      $("#start-the-party").removeAttr("disabled");
+      return null;
     }else{
       $("#player").append("<object type='application/x-shockwave-flash' data='http://grooveshark.com/songWidget.swf' width='0' height='1'><param name='wmode' value='window' /><param name='allowScriptAccess' value='always' /><param name='flashvars' value='hostname=cowbell.grooveshark.com&songIDs=" + song.grooveshark_id + "&style=metal&p=1' /></object>");
       $.post('/startsong', {song_id: song.id, party_id: window.location.href.split("/")[4]});
