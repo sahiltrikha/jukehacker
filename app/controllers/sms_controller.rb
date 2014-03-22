b900b5917d56c3d577b6cc62cde0fe902e639241
renagadex2 <etan.berkowitz@gmail.com>
2014-03-22 18:06:00 -0400

    multi party activated

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -39,33 +39,6 @@ class SmsController < ApplicationController
       else
         reply("Don't send us garbage!")
       end
-
-      # guest = Guest.find_by(user_id: @current_user.id)
-      # party_id = guest.party_id
-      # party = Party.find_by(id: party_id)
-
-      # if guest.nil? == false && Time.now < party.party_expiry #User is in a party (they have been validadted with a party key)
-      #   current_guest_info = Guest.find_by(user_id: @current_user.id)
-      #   current_party_id = current_guest_info.party_id
-      #   current_party = Party.find_by(id: current_party_id)
-      #   reply("Your song, #{@current_message_body}, has been added to the queue")
-      #   getGrooveshark("#{@current_message_body}", current_party_id, @current_user.id)
-      # elsif guest.nil? == false && Time.now > party.party_expiry
-      #     reply("go home brah, no more songs for you.")
-      # else #User is not in a party (they are not a guest but they are a user in the database)
-      #   if Party.find_by(party_key: @current_message_body).nil? == false #the user's message is #wagparty
-      #     current_party = Party.find_by(party_key: @current_message_body)
-      #     if Time.now > current_party.party_expiry
-      #       reply("go home brah, party over.")
-      #     else
-      #       Guest.create(user_id: @current_user.id, party_id: current_party.id)#find party by message and add user
-      #       reply("Congrats! You've Joined #{current_party.party_key}")#reply "CONGRATS HOMIE. reply for requests"
-      #     end
-      #   else #the user's message is rando
-      #     reply("You mad bro?")
-      #   end
-      # end
-      # # end
     end
 
     render ('index')
