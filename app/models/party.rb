69ed81d10b00f67ad588debca0a1529b2dd6ea64
zstayman <z.stayman@gmail.com>
2014-03-22 15:19:24 -0400

    user associations set

--- app/models/party.rb
+++ app/models/party.rb
@@ -3,5 +3,6 @@ class Party < ActiveRecord::Base
   has_many :past_songs, through: :played_songs, source: :song
   has_many :queued_songs
   has_many :played_songs
+  belongs_to :host
   validates :party_key, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,5 @@ require 'spec_helper'
 
 describe User do
   it { should have_one(:host) }
+  it { should have_many(:guests)}
 end
