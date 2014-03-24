8bd633b4b1ed3156461ba2ccb65efa4451550dc1
zstayman <z.stayman@gmail.com>
2014-03-24 18:43:40 -0400

    rules validated

--- app/models/rule.rb
+++ app/models/rule.rb
@@ -1,3 +1,5 @@
 class Rule < ActiveRecord::Base
   has_and_belongs_to_many :parties
+  validates :condition, presence: true, uniqueness: true
+  validates :banned_value, presence: true, uniqueness: true
 end
