73ce4793e3adba517833f5f25a12fc583f551606
Sahil Trikha <trikha93@gmail.com>
2014-03-25 17:59:01 -0400

    party page updated

--- app/assets/javascripts/queueDisplay.js
+++ app/assets/javascripts/queueDisplay.js
@@ -1,11 +1,11 @@
 var getQueue = function(){
   // getqueue retrieves a list of all songs in the queue for this party
   $.getJSON('/getqueue', function(response){
-    // this removes the previous version of the playlist
-    $("ol").children().remove()
     // this iterates over the pending playlist and adds them to the playlist
-    response.forEach(function(song){
-      $("<li>").text("Song: " + song.title + ", Artist: " + song.artist).addClass("queue-item").fadeIn(50).appendTo("#song-playlist");
+    response.forEach(function(song, index){
+      console.log(index);
+      $("#queue-" + index.toString()).children().remove()
+      $("#queue-" + index.toString()).html("<img class='queue-art' src='http://a1.mzstatic.com/us/r30/Music/17/07/36/mzi.ldnorbao.100x100-75.jpg' /><span class='queue-text'><p>Title: " + song.title + "</p><p>Artist: " + song.artist + "</p></span>");
     });
   });
 };
@@ -13,6 +13,15 @@ var getQueue = function(){
 if(document.querySelector("#song-playlist")){
   $("#song-playlist").load(getQueue());
   $("#song-playlist").ready(function(){
-    setInterval('getQueue()', 1000);
+    setInterval('getQueue()', 100000);
   });
 };
+  // <li>
+          // <div class='queue'>
+          //     <img class='queue-art' src='http://a1.mzstatic.com/us/r30/Music/17/07/36/mzi.ldnorbao.100x100-75.jpg' />
+          //     <span class='queue-text'>
+          //         <p>Title: " + song.title + "</p>
+          //         <p>Artist: " + song.artist + "</p>
+          //     </span>
+          // </div>
+          // </li>
