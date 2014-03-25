815e6c2e891994fb221673654d0c8c028fb565d8
Michael K. Wagner <michael.k.wagner@gmail.com>
2014-03-25 17:16:20 -0400

    Add Auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def current_host
+    @current_host= session[:host_id]
+  end
+
+  def logged_in?
+    current_host.present?
+  end
+
+  helper_method :current_host, :logged_in?
 end
